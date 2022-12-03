2f75e07bcdfa599fe42ae361f73e584ec3c0f168
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-12-02 23:44:02 -0500

    trabajando interfaz de login

--- administrador/index.php
+++ administrador/index.php
@@ -1,14 +1,52 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
     <title>Administrador del sitio web</title>
-</head>
-<body>
-    
-    Login
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-</body>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+            <div class="card">
+                <div class="card-header">
+                    Login
+                </div>
+                <div class="card-body">
+                    <form>
+                    <div class = "form-group">
+
+                        <label for="exampleInputEmail1">Usuario</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresa tu usuario">
+
+                    </div>
+
+                    <div class="form-group">
+
+                        <label for="exampleInputPassword1">Contraseña:</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Ingresa tu contraseña">
+
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Entrar al administrador</button>
+                    </form>
+                    
+                    
+                </div>
+            </div>
+                
+            </div>
+            
+        </div>
+    </div>
+      
+<!-- me quede min 47:15 -->
+  </body>
 </html>
