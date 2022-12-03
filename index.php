9025d2df3bd1cfb1451923e2f9281d0bc46cfd14
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-12-02 22:50:05 -0500

    creando la estructura básica

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-    echo "Hola";
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,8 +5,50 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sitio web</title>
+
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
 </head>
 <body>
-    <!-- me quede en el min 15:17 -->
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">BookDevs</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Inicio</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Libros</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nosotros</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+
+
+            <div class="jumbotron">
+                <h1 class="display-3">Jumbo heading</h1>
+                <p class="lead">Jumbo helper text</p>
+                <hr class="my-2">
+                <p>More info</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                </p>
+            </div>
+
+
+        </div>
+    </div>
+
+<!-- me quede en el min 15:17 -->
+    
 </body>
 </html>
