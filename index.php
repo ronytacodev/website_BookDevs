5ebef0f2b812c30644eeceba7968a088d08417e6
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-12-02 23:01:26 -0500

    agregando cabecera, pie y nosotros.php

--- index.php
+++ index.php
@@ -1,38 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sitio web</title>
-
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-</head>
-<body>
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <ul class="nav navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="#">BookDevs</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">Inicio</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">Libros</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">Nosotros</a>
-            </li>
-        </ul>
-    </nav>
-
-    <div class="container">
-        <div class="row">
-
+<?php include("template/cabecera.php");?>
 
             <div class="jumbotron">
                 <h1 class="display-3">Jumbo heading</h1>
@@ -44,11 +10,4 @@
                 </p>
             </div>
 
-
-        </div>
-    </div>
-
-<!-- me quede en el min 15:17 -->
-    
-</body>
-</html>
+<?php include("template/pie.php");?>
