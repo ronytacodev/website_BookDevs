b09a2e48b8d2712ab898befcde427cd1439991bb
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-12-05 11:12:46 -0500

    creando cabecera y pie del administrador

--- administrador/inicio.php
+++ administrador/inicio.php
@@ -1,28 +1,6 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-      
-    <nav class="navbar navbar-expand navbar-light bg-light">
-        <div class="nav navbar-nav">
-            <a class="nav-item nav-link active" href="#">Administrador del sitio web <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">Inicio</a>
-            <a class="nav-item nav-link" href="#">Libros</a>
-            <a class="nav-item nav-link" href="#">Cerrar sesión</a>
-            <a class="nav-item nav-link" href="#">Ver sitio web</a>
-        </div>
-    </nav>
+<?php include('template/cabecera.php');?>
 
-    <div class="container">
-        <div class="row">
             <div class="col-md-12">
                 
                 <div class="jumbotron">
@@ -37,8 +15,4 @@
 
             </div>
             
-        </div>
-    </div>
-
-  </body>
-</html>
+<?php include('template/pie.php');?>
---
+++ administrador/template/cabecera.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Administrador</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+      
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <div class="nav navbar-nav">
+            <a class="nav-item nav-link active" href="#">Administrador del sitio web <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Inicio</a>
+            <a class="nav-item nav-link" href="#">Libros</a>
+            <a class="nav-item nav-link" href="#">Cerrar sesión</a>
+            <a class="nav-item nav-link" href="#">Ver sitio web</a>
+        </div>
+    </nav>
+
+    <div class="container">
+    <br/>    
+        <div class="row">
