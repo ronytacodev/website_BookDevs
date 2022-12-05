091847868b28f145d29acedafa7759e57baf16c9
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-12-05 11:35:05 -0500

    agregando links al menú

--- administrador/template/cabecera.php
+++ administrador/template/cabecera.php
@@ -10,14 +10,19 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
+
+    <?php $url="http://".$_SERVER['HTTP_HOST']."/sitioweb" ?>
       
     <nav class="navbar navbar-expand navbar-light bg-light">
         <div class="nav navbar-nav">
             <a class="nav-item nav-link active" href="#">Administrador del sitio web <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">Inicio</a>
-            <a class="nav-item nav-link" href="#">Libros</a>
-            <a class="nav-item nav-link" href="#">Cerrar sesión</a>
-            <a class="nav-item nav-link" href="#">Ver sitio web</a>
+            <a class="nav-item nav-link" href="<?php echo $url; ?>/administrador/inicio.php">Inicio</a>
+
+            <a class="nav-item nav-link" href="<?php echo $url; ?>/administrador/seccion/productos.php">Libros</a>
+            
+            <a class="nav-item nav-link" href="<?php echo $url; ?>/administrador/seccion/cerrar.php">Cerrar sesión</a>
+            
+            <a class="nav-item nav-link" href="<?php echo $url; ?>">Ver sitio web</a>
         </div>
     </nav>
 
