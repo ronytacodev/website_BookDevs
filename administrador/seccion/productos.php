1f739680e20c3774f12c64518f2b3e36e406322e
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-12-05 11:41:54 -0500

    incluyendo cabecera y pie para sección PRODUCTOS

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -1,1 +1,11 @@
-Libros
+<?php include("../template/cabecera.php"); ?>
+
+<div class="col-md-5">
+    Formulario de agregar libros
+</div>
+
+<div class="col-md-7">
+    Tabla de libros
+</div>
+
+<?php include("../template/pie.php"); ?>
