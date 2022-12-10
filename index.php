6c55ae9c68d6632d4d01483bee75ca7ec19c2561
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-12-10 17:44:15 -0500

    mas ajustes finales

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
             <div class="jumbotron text-center">
                 <h1 class="display-3">Bienvenidos a Bookdevs</h1>
-                <p class="lead">Consulta libros de programación.</p>
+                <p class="lead">Contamos con una variedad para que aprendas desarrollo web en todo su esplendor.</p>
                 <hr class="my-2">
                 <img width="400" src="img/banner.jpg" class="img-thumbnail rounded mx-auto d-block"/>
 
--- template/cabecera.php
+++ template/cabecera.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sitio web</title>
+    <link rel="shortcut icon" href="../img/logo_bookdevs.ico" type="image/x-icon">
+    <title>BookDevs</title>
 
     <link rel="stylesheet" href="./css/bootstrap.min.css">
 </head>
