731475ec6d92d98991d617aded0837f308b889fa
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-12-10 07:59:44 -0500

    code para productos.php del sitio web para mostrar los libros

--- productos.php
+++ productos.php
@@ -1,44 +1,27 @@
 <?php include("template/cabecera.php");?>
 
-<div class="col-md-3">
-<div class="card">
-    <img class="card-img-top" src="./img/libro1.jpg" alt="">
-    <div class="card-body">
-        <h4 class="card-title">Libro php</h4>
-        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
-    </div>
-</div>
-</div>
+<?php
+    include("administrador/config/bd.php");
 
-<div class="col-md-3">
-<div class="card">
-    <img class="card-img-top" src="./img/libro1.jpg" alt="">
-    <div class="card-body">
-        <h4 class="card-title">Libro php</h4>
-        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
-    </div>
-</div>
-</div>
+    $sentenciaSQL = $conexion->prepare("SELECT * FROM libros");
+    $sentenciaSQL->execute();
+    $listalibros = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
 
-<div class="col-md-3">
-<div class="card">
-    <img class="card-img-top" src="./img/libro1.jpg" alt="">
-    <div class="card-body">
-        <h4 class="card-title">Libro php</h4>
-        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
-    </div>
-</div>
-</div>
+?>
 
-<div class="col-md-3">
-<div class="card">
-    <img class="card-img-top" src="./img/libro1.jpg" alt="">
-    <div class="card-body">
-        <h4 class="card-title">Libro php</h4>
-        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+<?php foreach($listalibros as $libro) { ?>
+
+    <div class="col-md-3">
+        <div class="card">
+            <img class="card-img-top" src="./img/<?php echo $libro['imagen']; ?>" alt="">
+            <div class="card-body">
+                <h4 class="card-title"><?php echo $libro['nombre']; ?></h4>
+                <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+            </div>
+        </div>
     </div>
-</div>
-</div>
+
+<?php } ?>
 
 
 <?php include("template/pie.php");?>
