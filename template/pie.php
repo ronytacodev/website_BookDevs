7d8b3ef876e5a64d0305db47219b20c3bd286f4a
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-12-12 09:52:07 -0500

    trabajando en el footer

--- template/pie.php
+++ template/pie.php
@@ -1,6 +1,29 @@
             
             </div>
     </div>
+
+    <!-- INICIO SECCION FOOTER -->
+    <section id="footer" class="bg-primary">
+    <div class="container">
+        <ul class="list-inline">
+            <li class="list-inline-item footer-menu">
+                <a href="#hero">Inicio</a>
+            </li>
+            <li class="list-inline-item footer-menu">
+                <a href="#portfolio">Libros</a>
+            </li>
+            <li class="list-inline-item footer-menu">
+                <a href="#aboutus">Nosotros</a>
+            </li>
+            <li class="list-inline-item footer-menu">
+                <a href="#pricing">Login</a>
+            </li>
+        </ul>
+
+        <small>©2022 All Rights Reserved. Created by Rony Taco</small>
+    </div>
+</section>
+<!-- FIN SECCION FOOTER -->
     
 </body>
 </html>
