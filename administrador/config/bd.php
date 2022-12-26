46e571030be13937728d8e41d8c8c188ebb04cca
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-12-25 22:15:10 -0500

    ingresando credenciales para los servidores 000webhost y el de infinity free

--- administrador/config/bd.php
+++ administrador/config/bd.php
@@ -1,8 +1,16 @@
 <?php
-    // $host="sql303.epizy.com:33065";
+// PARA INFINITY FREE
+    // $host="sql303.epizy.com";
     // $bd="epiz_33257783_sitio";
     // $usuario="epiz_33257783";
     // $contrasenia="HjLUwKSf9iySZ1K";
+
+// PARA 000WEBHOST
+    // $host="localhost";
+    // $bd="id20060482_sitio";
+    // $usuario="id20060482_adminrony";
+    // $contrasenia="hNw0ShQH\@6(%@s*";
+
 
     // PARA SERVER LOCAL USAR CREDENCIALES ABAJO
     $host="localhost:33065";
