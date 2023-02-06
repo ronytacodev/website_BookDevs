1f0a8f90f2eedf76471ddb91be6db1fcc4ccf089
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-02-06 10:03:13 -0500

    gitignore

--- administrador/config/bd.php
+++ administrador/config/bd.php
@@ -1,16 +1,4 @@
 <?php
-// PARA INFINITY FREE
-    // $host="sql303.epizy.com";
-    // $bd="epiz_33257783_sitio";
-    // $usuario="epiz_33257783";
-    // $contrasenia="HjLUwKSf9iySZ1K";
-
-// PARA 000WEBHOST
-    // $host="localhost";
-    // $bd="id20060482_sitio";
-    // $usuario="id20060482_adminrony";
-    // $contrasenia="hNw0ShQH\@6(%@s*";
-
 
     // PARA SERVER LOCAL USAR CREDENCIALES ABAJO
     $host="localhost:33065";
@@ -27,5 +15,3 @@
     
         echo $ex->getMessage();
     }
-?>
-
